765551ce7a5314a5867c7a6cbfc8adba48aa6e35
Hanee Haresh Patel <haneepatelkadi@gmail.com>
2022-06-27 23:53:34 -0700

    changes in post.js

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -1,5 +1,5 @@
 import PostMessage from '../models/postMessage.js';
-export const getPosts = (req, res) => {
+export const getPosts = async (req, res) => {
     try {
         const postMessage = await PostMessage.find();
         console.log(postMessage);
@@ -7,6 +7,16 @@ export const getPosts = (req, res) => {
     } catch (error) {
         res.status(404).json({ message: error.message });
     }
-export const createPost = (req, res) => {
+}
+export const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostMessage(post);
+    try {
+        await newPost.save();
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
     res.send('Post Creation');
+
 }
